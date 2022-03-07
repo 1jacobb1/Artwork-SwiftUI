6b87cd09cae89b3f76458878fbd55334d9c6b237
Jacob <jacob_potolin@ligph.com>
2022-03-07 10:46:25 +0800

    Fixed untracked files

--- Pods/Target Support Files/Pods-Artwork-SwiftUI-Artwork-SwiftUIUITests/Pods-Artwork-SwiftUI-Artwork-SwiftUIUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Artwork_SwiftUI_Artwork_SwiftUIUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Artwork_SwiftUI_Artwork_SwiftUIUITests
-@end
